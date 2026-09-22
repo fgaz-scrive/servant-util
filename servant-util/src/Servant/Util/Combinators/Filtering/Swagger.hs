e6b6ddffc784635fb86a7b1da853d06ea739e13e
Francesco Gazzetta <francesco.gazzetta@scrive.com>
2026-09-22 12:06:13 +0200

    Fix lexical error
    CPP can't handle multiline strings.

--- servant-util/src/Servant/Util/Combinators/Filtering/Swagger.hs
+++ servant-util/src/Servant/Util/Combinators/Filtering/Swagger.hs
@@ -69,8 +69,8 @@ autoFilterDesc ops = fullDesc
     opsDesc
         | [(DefFilteringCmd, _)] <- ops = []
         | otherwise =
-            "You can specify a custom filtering operation in `param[op]=value` \
-            \or `param_op=value` format." :
+            ("You can specify a custom filtering operation in `param[op]=value` " ++
+            "or `param_op=value` format.") :
             "Allowed operations:" :
             (ops <&> \(op, engDesc) -> "* `" <> op <> "` (" <> engDesc <> ")")
 
